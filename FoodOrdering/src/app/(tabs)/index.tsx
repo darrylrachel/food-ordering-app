bdeacc4993faa32f442d41956414de7f20f3e652
darrylrachel <racheldarryl@icloud.com>
2025-04-03 05:35:03 -0700

    Product component

--- FoodOrdering/src/app/(tabs)/index.tsx
+++ FoodOrdering/src/app/(tabs)/index.tsx
@@ -1,39 +1,15 @@
-import { Text, View, StyleSheet, Image } from 'react-native';
-import Colors from '@/src/constants/Colors';
+import { View} from 'react-native';
 import products from '@/assets/data/products';
+import ProductListItem from '@/src/components/ProductListItem';
 
-const product = products[0];
 
-export default function TabOneScreen() {
+
+export default function MenuScreen() {
   return (
-    <View style={styles.container}>
-      <Image style={styles.image} source={{uri: product.image}} />
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>
+    <View>
+      <ProductListItem product={products[0]} />
+      <ProductListItem product={products[3]} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#fff',
-    padding: 10,
-    borderRadius: 10,
-  },
-
-  title: {
-    fontSize: 20,
-    fontWeight: '600',
-    marginVertical: 10
-  },
-  
-  price: {
-    color: Colors.light.tint,
-    fontWeight: 'bold'
-  },
-
-  image: {
-    width: '100%',
-    aspectRatio: 1,
-  }
-});

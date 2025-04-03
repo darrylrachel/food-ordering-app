9b780441010f51f8c1c41f9af570b942d2d96769
darrylrachel <racheldarryl@icloud.com>
2025-04-03 10:04:52 -0700

    Product details page

--- FoodOrdering/src/app/(tabs)/menu/_layout.tsx
+++ FoodOrdering/src/app/(tabs)/menu/_layout.tsx
@@ -1,5 +1,10 @@
 import { Stack } from "expo-router";
 
 export default function MenuStack () {
-  return <Stack />
+  return <Stack>
+    <Stack.Screen 
+      name="index"
+      options={{ title: 'Menu'}}
+    />
+  </Stack>
 }
